000c6ce938905b8a41ddf2778d3c57f859308328
Clinton <80900245+pravton@users.noreply.github.com>
2021-09-09 12:05:50 -0400

    completed form functionalities

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,3 +24,49 @@ export const ADD_USER = gql`
     }
   }
 `
+
+export const ADD_FRIEND = gql`
+  mutation addFriend($id: ID!) {
+    addFriend(friendId: $id) {
+      _id
+      username
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`
+// add thought
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+
+// add reaction
+export const ADD_REACTION = gql`
+  mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+    addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`
